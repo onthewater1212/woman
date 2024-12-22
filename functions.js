947171c2e4b6919319b5bc0cbbe3d1597427a753
onthewater1212 <moe.mizukami.8i@stu.hosei.ac.jp>
2024-12-22 23:49:44 +0900

    ok

---
+++ functions.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const imageContainer = document.querySelector('.image-container');
+const layers = document.querySelectorAll('.image>*');
+
+gsap.set(layers, {opacity: 0});
+gsap.set('#layer01', {opacity: 1});
+
+ScrollTrigger.create({
+    trigger:imageContainer,
+    start: 'top top',
+    end: '+=2000',
+    pin: true,
+    scrub: true,
+    //markers: true,
+    onUpdate: (self) => {
+        if (self.progress < 0.25) {
+          gsap.to('#layer01',{ opacity: 1});
+          gsap.to('#layer02, #layer03, #layer04', {opacity: 0 });
+        } else if (self.progress>=0.25 && self.progress < 0.5) {
+        　gsap.to('#layer01, #layer02',{ opacity: 1 });
+          gsap.to('#layer03, #layer04',{ opacity: 0 });
+        } else if (self.progress >= 0.5 && self.progress < 0.75) {
+          gsap.to('#layer01, #layer02, #layer03' , {opacity: 1 });
+          gsap.to('#layer04', {opacity: 0 });
+        } else {
+            gsap.to('#layer01, #layer02, #layer03, #layer04', {opacity: 1 })
+        }
+    },
+});
